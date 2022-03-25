9ed2adab8ece77985f45bcacf876fb9203924d17
Alex Bergers <bergersj@mail.gvsu.edu>
2022-03-25 18:47:24 -0400

    Reaction Time with red and green change

--- src/main/java/hbmpack/reactionTime.java
+++ src/main/java/hbmpack/reactionTime.java
@@ -2,8 +2,7 @@ package hbmpack;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
 
 public class reactionTime extends JFrame implements ActionListener {
     static JFrame reactionTime = new JFrame();
@@ -12,66 +11,75 @@ public class reactionTime extends JFrame implements ActionListener {
     JLabel click = new JLabel();
     JButton start = new JButton("Start");
     JButton home_but = new JButton();
-    JButton redBut = new JButton("test");
+    JButton redBut = new JButton();
+    JPanel panel = new JPanel();
     Timer delay;
+    JLabel results = new JLabel();
+
+    int elapsedTime = 0;
+    boolean test= false;
+    int randomDelay = 5000 + (int)(Math.random()*((15000 - 5000)+1));
 
     HomePage.Profile reactCurrent;
 
-    reactionTime(HomePage.Profile current) {
+    public reactionTime(HomePage.Profile current) {
 
         reactCurrent = current;
 
-        reactionTime.setTitle("Reaction Time"); // Sets Title
-        reactionTime.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit Application
-        reactionTime.setLayout(null);
-        reactionTime.setSize(600, 600); //set dimension
-        reactionTime.setVisible(true); //make frame visible
+        this.setTitle("Reaction Time"); // Sets Title
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Exit Application
+        this.setLayout(null);
+        this.setSize(600, 600); //set dimension
+        this.setVisible(true); //make frame visible
+
+        panel.setBounds(0,0,600,600);
+        panel.setVisible(false);
+        panel.setLayout(null);
+        this.add(panel);
+
+        results.setText("Too Soon! Press Try Again to restart!");
+        results.setBounds(100,200,600,100);
+        results.setVisible(false);
+        results.setFont((new Font("Serif", Font.PLAIN, 25)));
+        this.add(results);
 
         title.setText("Reaction Time Test");
         title.setHorizontalAlignment(SwingConstants.CENTER);
         title.setBounds(0, 150, 600, 100);
         title.setFont(new Font(null, Font.PLAIN, 40));
         title.setVisible(true);
-        reactionTime.add(title);
+        this.add(title);
 
         subtitle.setText("When the red box turns green, click as quickly as you can!");
         subtitle.setHorizontalAlignment(SwingConstants.CENTER);
         subtitle.setBounds(0, 200, 600, 100);
         subtitle.setFont(new Font(null, Font.PLAIN, 14));
         subtitle.setVisible(true);
-        reactionTime.add(subtitle);
+        this.add(subtitle);
 
         click.setText("Click the start button to begin");
         click.setHorizontalAlignment(SwingConstants.CENTER);
         click.setBounds(0, 230, 600, 100);
         click.setFont(new Font(null, Font.PLAIN, 14));
         click.setVisible(true);
-        reactionTime.add(click);
+        this.add(click);
 
         start.setHorizontalAlignment(SwingConstants.CENTER);
         start.setBounds(250, 300, 100, 50);
         start.addActionListener(this);
         start.setVisible(true);
-        reactionTime.add(start);
+        this.add(start);
 
         // add home button
-        reactionTime.add(home_but);
+        this.add(home_but);
         home_but.setBounds(220, 475, 150, 50); // sets location and size of button
         home_but.setFocusable(false);
         home_but.setText("Return to Menu");
         home_but.addActionListener(this);
 
-        redBut.setBounds(0,0,600,600);
-        redBut.setVisible(false);
-        redBut.setFocusable(true);
-        redBut.addActionListener(this);
-        redBut.setBackground(Color.red);
-        redBut.setOpaque(true);
-        redBut.setBorder(null);
-        reactionTime.add(redBut);
-
-
     }
+    Timer timer = new Timer(1000, e -> elapsedTime+=1000);
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == home_but) {
             new HomePage(reactCurrent);
@@ -80,23 +88,53 @@ public class reactionTime extends JFrame implements ActionListener {
         if (e.getSource() == start){
             theGame();
         }
-        if (e.getSource() == start){
-
+        if (e.getSource() == redBut) {
+            if (!test) {
+                results.setVisible(true);
+                panel.setVisible(false);
+                start.setText("Try Again");
+                start.setVisible(true);
+                home_but.setVisible(true);
+            }
+            if (test) {
+                timer.stop();
+                panel.setVisible(false);
+                results.setVisible(true);
+                start.setText("Try Again");
+                start.setVisible(true);
+                home_but.setVisible(true);
+                results.setText("Time: " + elapsedTime);
+            }
         }
-
+        if (e.getSource() == delay){
+            redBut.setBackground(Color.GREEN);
+            test = true;
+            timer.start();
+        }
     }
 
+
     public void theGame(){
+        redBut.setBounds(0,0,600,600);
+        redBut.setVisible(false);
+        redBut.setFocusable(true);
+        redBut.setOpaque(true);
+        redBut.setBackground(Color.red);
+        redBut.setBorderPainted(false);
+        redBut.addActionListener(this);
+        panel.add(redBut);
         redBut.setVisible(true);
+
+        panel.setVisible(true);
         start.setVisible(false);
         click.setVisible(false);
         title.setVisible(false);
         subtitle.setVisible(false);
         home_but.setVisible(false);
 
-        //delay = new Timer(5000, this);
-        //delay.setRepeats(false);
-        //delay.start();
-
+        delay = new Timer(randomDelay, this);
+        delay.setRepeats(true);
+        delay.start();
     }
+
 }
